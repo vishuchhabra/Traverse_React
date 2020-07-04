af73cb0d0612da1ac743f4b54f9bc6ef3c9c292c
vishuchhabra <vishuchhabra1016@gmail.com>
2020-07-04 20:24:41 +0530

    In Progress

---
+++ ReactAsStaticWithBackendServer/build/precache-manifest.ed02271c9275a9ce14a17219de3be443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b95d1e97d17bd48631018b9260c6574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1307c4a6bb09f204defa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e16f13e762fb5aca21a3",
+    "url": "/static/js/2.0e6e49c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e6e49c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1307c4a6bb09f204defa",
+    "url": "/static/js/main.1f3f4002.chunk.js"
+  },
+  {
+    "revision": "32c4474eb0c6712e2ddd",
+    "url": "/static/js/runtime-main.aea97cb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
