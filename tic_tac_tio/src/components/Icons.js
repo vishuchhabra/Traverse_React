77423d931ae350faae763d8b11fc71f56c90a2d1
vishuchhabra <vishuchhabra1016@gmail.com>
2020-08-14 18:33:23 +0530

    One more

---
+++ tic_tac_tio/src/components/Icons.js
@@ -0,0 +1,20 @@
+import React from  'react'
+import {FaTimes,FaPen, FaRegCircle} from  'react-icons/fa'
+
+const Icon =({name})=>{
+    switch(name){
+        case 'circle':
+            return <FaRegCircle className="icons"/>
+            break;   
+        
+        case 'cross':
+            return <FaTimes className="icons"/>
+            break;   
+    
+        default :
+            return <FaPen className="icons"/> 
+    }
+
+}
+
+export default Icon 
