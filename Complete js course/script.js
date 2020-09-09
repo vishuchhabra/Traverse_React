7779e7caca3e751457648360da502e6d33a4a931
vishuchhabra <vishuchhabra1016@gmail.com>
2020-09-09 15:39:43 +0530

    Working

---
+++ Complete js course/script.js
@@ -0,0 +1,70 @@
+// //important terms in  the java script 
+
+// //undefined
+// var  user;
+// console.log(user)
+
+// // null
+// user =null
+// if(user){
+//     console.log("If is working")
+// }
+// // NaN
+
+// // 0
+
+// // ""
+
+// //falsy values are
+// // undefined null "" 0 NaN
+
+// difference between == and ===
+
+// var val ="2"
+
+// if(2==val){
+//     console.log("First If is working")
+// }
+
+// if(2===val){
+//     console.log("Second if is working")
+// }
+
+
+
+// Functions in java script 
+
+// function  hello(){
+//     console.log("Hello  there hitesh")
+// }
+
+// hello()
+
+// var func = function(name){
+//     console.log(`your name is ${name}`)
+// }
+
+
+// func("vishu chhabra")
+
+//same above using arrow funtion 
+// var func = (name)=>{
+//     console.log(`your name is ${name}`)
+// }
+
+
+// func("vishu chhabra")
+
+// var  first ;
+// console.log(first)
+
+// first ="vishu chhabra"
+// console.log(first);
+
+
+//for not a number (NaN)
+// console.log(parseInt("a"))
+
+
+
+ 
