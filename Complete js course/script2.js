1fc39da1fab7c3f8c1d4f876fe1485cab2348979
vishuchhabra <vishuchhabra1016@gmail.com>
2020-09-10 11:46:08 +0530

    Working fine

---
+++ Complete js course/script2.js
@@ -0,0 +1,33 @@
+//call back function  in  every function 
+
+// var result = [1,2,4,6,8].every((num)=>num%2==0)
+
+// console.log(result)
+
+//Fill  and Filter in  the java script 
+
+// var testArray =[2,4,6,3,1,5,9,8]
+
+// console.log(testArray.fill(0))  //for filling the whole array with any specific number, position we can also define in this fucntion.
+ 
+
+//filter function 
+
+// var test =[1,2,3,4,5,6,7]
+
+// console.log(test.filter((num)=>(!(num%2))))
+
+
+//slice in java script 
+ 
+// var user =["vishu","karan","hitesh","ritesh","prajuwal"]
+// // console.log(user.slice(0,6))
+
+
+// //spplice 
+
+// user.splice(0,3,"reetu")
+
+// console.log(user);
+
+
---
+++ Complete js course/script3.js
@@ -0,0 +1,13 @@
+// object in  java script 
+
+var user = {
+    firstName :"vishu chhabra",
+    lastName :"chhabra",
+    role:"Admin",
+    loginCount:32,
+    facebookSignedIn: true
+}
+
+console.log(user.firstName)
+
+console.table(user)
